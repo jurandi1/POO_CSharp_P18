c21d8c11d4eb7785c3c998a6f9cee18e30faaeea
Jurandi Junior <jurandijr20@gmail.com>
2025-02-14 22:30:12 -0300

    Interface IComparable - PROBLEMA 1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,29 @@
     {
         private static void Main(string[] args)
         {
+            //PROBLEMA 1 -  Interface IComparable
+            string path = @"C:\Windows\Temp\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
