230028a21eff8de2aed4bb8556209aa7030ab03f
Jurandi Junior <jurandijr20@gmail.com>
2025-02-14 22:46:18 -0300

    Interface IComparable - PROBLEMA 2

--- Program.cs
+++ Program.cs
@@ -1,11 +1,49 @@
-﻿namespace POO_CSharp_P18
+﻿using System;
+using POO_CSharp_P18.Entities;
+
+namespace POO_CSharp_P18
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            //PROBLEMA 1 -  Interface IComparable
-            string path = @"C:\Windows\Temp\in.txt";
+            //Interface IComparable - PROBLEMA 2
+            string path = @"c:\Windows\Temp\in2.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+
+
+
+
+
+
+
+            //Interface IComparable - PROBLEMA 1 
+            /*
+            string path = @"C:\Windows\Temp\in1.txt";
             try
             {
                 using (StreamReader sr = File.OpenText(path))
@@ -27,6 +65,7 @@
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
+            */
         }
     }
 }
